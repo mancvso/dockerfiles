6edd9ae41154e3403652e455d26c153212319d6c
Joseph Versoza <jversoza@xsmail.com>
2014-08-01 23:30:46 -0400

    updated to docker 7 with ubuntu 14.04 lts, along with minor fixes to usernames/passes... and lastly, an updated readme

--- README.md
+++ README.md
@@ -1,6 +1,33 @@
-# Dockerfiles
-For use with [Docker](http://docker.io)
-See also:
-	http://dischord.org/blog/2013/07/10/docker-and-owncloud/
-	http://dischord.org/blog/2013/08/13/docker-and-owncloud-part-2/
+# Docker + ownCloud + boot2docker
+
+## Based off of original repository from:
+
+* [docker and owncloud part 1 at dischord.org](http://dischord.org/blog/2013/07/10/docker-and-owncloud/)
+* [docker and owncloud part 2 at dischord.org](http://dischord.org/blog/2013/08/13/docker-and-owncloud-part-2/)
+
+## Useful Docker commands
+
+<pre>
+# show docker vm ip
+boot2docker ip
+
+# run with ssl
+docker run -d -p 443:443 owncloud
+
+# show all containers
+docker ps -a 
+
+# stop container
+docerk stop $CONTAINER
+
+# remove container
+docker rm $CONTAINER
+
+# list images
+docker images
+
+# remove image
+docker rmi $IMAGE_NAME
+
+</pre>
 
--- owncloud/resources/start.sh
+++ owncloud/resources/start.sh
@@ -7,13 +7,13 @@ if [ ! -f /etc/apache2/ssl/server.key ]; then
 	DOMAIN=$(hostname)
 	export PASSPHRASE=$(head -c 128 /dev/urandom  | uuencode - | grep -v "^end" | tr "\n" "d")
 	SUBJ="
-C=UK
-ST=England
-O=Dischord
-localityName=Manchester
+C=US
+ST=New York
+O=None
+localityName=New York
 commonName=$DOMAIN
 organizationalUnitName=
-emailAddress=nick@dischord.org
+emailAddress=myemail@example.com
 "
 	openssl genrsa -des3 -out /etc/apache2/ssl/server.key -passout env:PASSPHRASE 2048
 	openssl req -new -batch -subj "$(echo -n "$SUBJ" | tr "\n" "/")" -key $KEY -out /tmp/$DOMAIN.csr -passin env:PASSPHRASE 
@@ -21,11 +21,6 @@ emailAddress=nick@dischord.org
 	openssl rsa -in $KEY.orig -out $KEY -passin env:PASSPHRASE
 	openssl x509 -req -days 365 -in /tmp/$DOMAIN.csr -signkey $KEY -out /etc/apache2/ssl/server.crt
 fi
-
-# Add our hostname and Docker-allocated IP address into /etc/hosts
-# HOSTLINE=$(echo $(ip -f inet addr show eth0 | grep 'inet' | awk '{ print $2 }' | cut -d/ -f1) $(hostname) $(hostname -s))
-# echo $HOSTLINE >> /etc/hosts
-# Disabled for now until this is resolved (lol): https://github.com/dotcloud/docker/issues/2267
 
 /usr/bin/mysqld_safe &
 /usr/sbin/apache2ctl -D FOREGROUND
